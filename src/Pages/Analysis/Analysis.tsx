f6b3ab1222a2930589b279118a359e0a4f9fab1d
Mr-Aceil <mimiracle022@gmail.com>
2026-03-21 23:40:27 +0100

    Added a custom logo and fixed the Analysis page

--- src/Pages/Analysis/Analysis.tsx
+++ src/Pages/Analysis/Analysis.tsx
@@ -1,10 +1,15 @@
 import '../../index.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from '../../Components/Nav-bar/nav'
 
 export default function Analysis() {
     return (
         <>
         <Sidebar abg='bg-blue' aicon='white' atext='text-white' ahover='hover:none'/>
+        
+        <section className='flex flex-col w-full leftpad gap-[30px] pb-[20px]'>
+            <Navbar name='Ananlysis'/>
+        </section>
         </>
     )
 }
